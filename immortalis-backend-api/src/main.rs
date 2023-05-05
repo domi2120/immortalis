d1705985e19a4a178e40f6098fd1f893835e2b1a
domi2120 <d.hofrichter@gmx.de>
2023-05-05 22:12:25 +0200

    fix some style issues

--- immortalis-backend-api/src/main.rs
+++ immortalis-backend-api/src/main.rs
@@ -1,12 +1,12 @@
 use immortalis_backend_common::data_transfer_models::video_with_downloads::VideoWithDownload;
 use immortalis_backend_common::database_models::{video::Video, download::Download};
-use immortalis_backend_common::schema::{videos, downloads};
+use immortalis_backend_common::schema::{videos};
 use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
 
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 use diesel_async::pooled_connection::deadpool::Pool;
 use diesel_async::{RunQueryDsl, AsyncPgConnection};
-use diesel::{BelongingToDsl, QueryDsl, TextExpressionMethods, ExpressionMethods, PgTextExpressionMethods};
+use diesel::{BelongingToDsl, QueryDsl, PgTextExpressionMethods};
 use diesel::GroupedBy;
 use serde::Deserialize;
 
@@ -18,12 +18,12 @@ async fn health() -> impl Responder {
 }
 
 #[derive(Deserialize)]
-struct searchQuery {
+struct SearchQuery {
     term: Option<String>,
 }
 
 #[get("/search")]
-async fn search(query: web::Query<searchQuery> ,app_state: web::Data<AppState>) -> impl Responder {
+async fn search(query: web::Query<SearchQuery> ,app_state: web::Data<AppState>) -> impl Responder {
 
     let mut conn = app_state.db_connection_pool.get().await.unwrap();
     let mut results = videos::table.into_boxed();
