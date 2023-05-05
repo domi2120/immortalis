e591b4dce9af8ea83bf780ad9ee7c91ce5b6610f
domi2120 <d.hofrichter@gmx.de>
2023-05-05 20:50:58 +0200

    add readme and test data generation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # immortalis
  
+
+## Getting Started
+* run `docker compose up db pgadmin` to start postgres + pgadmin
+* run `cd immortalis-backend-common && ~/.cargo/bin/diesel migration run` to run the migrations
+* run `cd ../immortalis-client && pnpm i && pnpm run dev` to run the client (localhost:3000)
+* run `cd ../immortalis-backend-api && cargo run` to run the api (localhost:8080, proxied through client at "localhost:3000/api")
+* go to [pgadmin](http://localhost:5050/browser/) and execute the following code for some initial data
+```
+INSERT INTO public.videos(
+	title, channel, views, upload_date, archived_date, duration, thumbnail_address, original_url)
+	VALUES
+	('Ghost - Rats (Official Music Video)', 'Ghost', 5000000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 265, 'https://img.youtube.com/vi/C_ijc7A5oAc/maxresdefault.jpg', 'https://www.youtube.com/watch?v=C_ijc7A5oAc'),
+	('I Am', 'Theocrary - Topic', 380000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 660, 'https://img.youtube.com/vi/vfc8EjDuYNw/maxresdefault.jpg', 'https://www.youtube.com/watch?v=vfc8EjDuYNw');
+	
+insert into public.downloads (video_id, title, value)
+select id, 'Download(1080p30)', 'Download(1080p30)'
+from public.videos
+
+insert into public.downloads (video_id, title, value)
+select id, 'Audio Only', 'Audio Only'
+from public.videos
+```
--- immortalis-backend-api/src/main.rs
+++ immortalis-backend-api/src/main.rs
@@ -33,71 +33,7 @@ async fn search(app_state: web::Data<AppState>) -> impl Responder {
         .zip(results)
         .map(|(dl, vid)| VideoWithDownload{downloads: dl, video: vid} )
         .collect::<Vec<VideoWithDownload>>();
-
-    let test_data =vec![
-        Video {
-            id: 4,
-            title: "Ghost - Rats (Official Music Video)".to_string(),
-            channel: "Ghost".to_string(),
-            views: 5000000,
-            upload_date: chrono::Utc::now().naive_utc(),
-            archived_date: chrono::Utc::now().naive_utc(),
-            duration: 265,
-            thumbnail_address: "https://img.youtube.com/vi/C_ijc7A5oAc/maxresdefault.jpg".to_string(),
-            original_url: "https://www.youtube.com/watch?v=C_ijc7A5oAc".to_string(),
-/*
-            downloads: vec![ Download {
-                id: 5,
-                video_id: 5,
-                title: "Download(1080p30)".to_string(),
-                value: "Download(1080p30)".to_string()
-            }, Download {
-                id: 6,
-                video_id: 5,
-                title: "Audio Only".to_string(),
-                value: "Audio Only".to_string()
-            }],
-            selected_download: 
-            Download {
-                id: 5,
-                video_id: 5,
-                title: "Download(1080p30)".to_string(),
-                value: "Download(1080p30)".to_string()
-            }
-*/
-        },
-        Video {
-            id: 5,
-            title: "I Am".to_string(),
-            channel: "Theocracy - Topic".to_string(),
-            views: 388000,
-            upload_date: chrono::Utc::now().naive_utc(),
-            archived_date: chrono::Utc::now().naive_utc(),
-            duration: 660,
-            thumbnail_address: "https://img.youtube.com/vi/vfc8EjDuYNw/maxresdefault.jpg".to_string(),
-            original_url: "https://www.youtube.com/watch?v=vfc8EjDuYNw".to_string(),
- /*http://localhost:5050/browser/
-            downloads: vec![ Download {
-                id: 5,
-                video_id: 5,
-                title: "Download(1080p30)".to_string(),
-                value: "Download(1080p30)".to_string()
-            }, Download {
-                id: 5,
-                video_id: 5,
-                title: "Audio Only".to_string(),
-                value: "Audio Only".to_string()
-            }],
-            selected_download: 
-            Download {
-                id: 5,
-                video_id: 5,
-                title: "Download(1080p30)".to_string(),
-                value: "Download(1080p30)".to_string()
-            }
-        */
-        }
-    ];
+    
     HttpResponse::Ok().json(videos_with_downloads)
 }
 
