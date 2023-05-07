34c56e1894d6e254e29cc81a776a61d0b368070b
domi2120 <d.hofrichter@gmx.de>
2023-05-07 15:43:47 +0200

    some style adjustments

--- immortalis-client/src/components/VideoEntry.vue
+++ immortalis-client/src/components/VideoEntry.vue
@@ -1,18 +1,21 @@
 <template>
     <v-container class="fill-height" v-if="props.video.value.title" >
         <v-spacer></v-spacer>
-        <v-col :cols="1" sm=3 class="pa-3">
+        <v-col :cols="2" sm=2 class="pa-3">
             <v-img :src="props.video.value.thumbnailAddress" class="d-flex align-end" >
               <v-chip class="d-float float-right " variant="elevated">
                 {{ new Date(props.video.value.duration * 1000).toISOString().slice(11, 19) }}
               </v-chip>
             </v-img>
           </v-col>
-          <v-col :cols="3">
+          <v-col :cols="4">
             <h2 >{{ props.video.value.title }}</h2>
             {{ props.video.value.channel }} <br>
             {{ numberToDelimetedString(props.video.value.views, ",") }} views · uploaded: {{ new Date(props.video.value.uploadDate).toLocaleDateString() }} · archived: {{ new Date(props.video.value.archivedDate).toLocaleDateString() }} <br>
+            <!-- temporarily removed the data for it doesn't exist yet -->
+            <!-- 
             <v-select :disabled="props.video.value.downloads.length < 1" label="Download" :items="props.video.value.downloads" v-model="props.video.value.selectedDownload" class="w-40" return-object/>
+            -->
             <v-btn :href="'/api/download/' + encodeURI(props.video.value.title) + '.mkv'" :disabled="props.video.value.status != 'Archived'" @click="download(video)" >Download</v-btn>
             <v-btn :href="props.video.value.originalUrl" class="ma-2">Watch Original</v-btn>
             <v-chip>{{ props.video.value.status }}</v-chip>
