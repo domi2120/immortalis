8c43572bde2ebdf29354ab9e44213a8db695ee5d
domi2120 <d.hofrichter@gmx.de>
2023-05-07 12:42:48 +0200

    add unique constraint for tracked_collection_urls

--- immortalis-backend-common/migrations/2023-05-06-191734_create_tracked_collections/up.sql
+++ immortalis-backend-common/migrations/2023-05-06-191734_create_tracked_collections/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tracked_collections (
     id int not null primary key generated always as identity,
-    url varchar NOT NULL,
+    url varchar NOT NULL UNIQUE,
     tracking_started_at timestamp without time zone NOT NULL DEFAULT now(),
     last_checked timestamp without time zone
 );
