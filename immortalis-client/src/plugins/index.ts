9e3dfc92d1f5e868354eddea64315174a59c395d
domi2120 <d.hofrichter@gmx.de>
2023-06-08 22:21:01 +0200

    add translation support

--- immortalis-client/src/plugins/index.ts
+++ immortalis-client/src/plugins/index.ts
@@ -13,10 +13,29 @@ import router from '../router'
 // Types
 import type { App } from 'vue'
 
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  locale: navigator.language, // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages: {
+    en: {
+      "search": "Search"
+    },
+    ja: {
+      "search": "探す"
+    },
+    de: {
+      "search": "Suchen"
+    }
+  }
+})
+
 export function registerPlugins (app: App) {
   loadFonts()
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(i18n)
 }
