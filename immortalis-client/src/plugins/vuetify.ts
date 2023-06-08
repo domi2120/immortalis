879fe225b460f542c90793fea08efef745582a19
domi2120 <d.hofrichter@gmx.de>
2023-06-08 23:34:53 +0200

    add vuetify translations

--- immortalis-client/src/plugins/vuetify.ts
+++ immortalis-client/src/plugins/vuetify.ts
@@ -12,8 +12,17 @@ import { VDataTable } from "vuetify/labs/VDataTable";
 // Composables
 import { createVuetify } from 'vuetify'
 
+
+// Translations provided by Vuetify
+import { en, de } from 'vuetify/locale'
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  locale: {
+    locale: navigator.language.slice(0,2),
+    fallback: "en",
+    messages: { en, de }
+  },
   components: {
     VDataTable
   },
