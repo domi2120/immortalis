95cf5631df681626d5b443e27d9aa5d7c391e4be
domi2120 <d.hofrichter@gmx.de>
2023-06-11 14:55:32 +0200

    sort of fix the navigation bar behaviour

--- immortalis-client/src/App.vue
+++ immortalis-client/src/App.vue
@@ -3,7 +3,7 @@
     <v-main>
       <v-app-bar>
         <v-app-bar-nav-icon @click="drawerOpened= !drawerOpened" />
-        <v-toolbar-title style="cursor: pointer" @click="$router.push({path: '/', query: { t: Date.now()}})">Immortalis</v-toolbar-title>
+        <v-toolbar-title style="cursor: pointer" @click="clickNavigationEntryHome" >Immortalis</v-toolbar-title>
         <v-text-field :label="$t('search')" append-inner-icon="mdi-magnify" v-model="searchText" clearable class="w-50 mt-5 h-80" @keydown.enter="$router.push({ path: '/search', query: {searchText: searchText, t: Date.now()}})">
         </v-text-field>
         <v-spacer></v-spacer>
@@ -13,9 +13,10 @@
       </v-app-bar>
       <v-navigation-drawer v-model="drawerOpened" permanent>
         <v-list nav :mandatory="true">
-          <v-list-item :value="'Home'" :title="$t('navigation.home')" @click="$router.push({path: '/', query: { t: Date.now()}})"/>
-          <v-list-item :value="'Scheduling'" :title="$t('navigation.schedules')" @click="$router.push('/scheduling')"/>
-          <v-list-item :value="'TrackedCollection'" :title="$t('navigation.trackedCollections')" @click="$router.push('/tracked-collections')"/>
+          <v-list-item id="home" :value="'Home'" :title="$t('navigation.home')" router :to="`/?t${Date.now()}`" exact />
+          <v-list-item :value="'Scheduling'" :title="$t('navigation.schedules')" router :to="'/scheduling'" exact />
+          <v-list-item :value="'TrackedCollection'" :title="$t('navigation.trackedCollections')" router :to="'/tracked-collections'" exact/>
+          
         </v-list>
       </v-navigation-drawer>
       <router-view :key="$route.fullPath"></router-view>
@@ -29,8 +30,13 @@ import router from './router';
 import { emitter } from '@/eventService';
 import { WebSocketEvent } from './models/webSocketEvent';
 import consts from './consts';
+import { VListItem } from 'vuetify/components/VList';
 
-const drawerOpened = ref(false);
+// basically, not clicking the list and working just with the "to" prop, causes the old list entry to still be selected when changing the route by clicking an entry
+// outside of the list (like the home button around the toolbar-title)
+const clickNavigationEntryHome = () => (document.querySelector("#home") as HTMLAnchorElement).click();
+
+const drawerOpened = ref(true);
 
 let searchText: Ref<string> = ref("");
 
